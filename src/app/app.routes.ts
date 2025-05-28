c9f9cd66320ede1bf13ed620d29d3ac954554ee6
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-05-28 20:33:18 +0800

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { BusinessSignupComponent } from '../components/business-signup/business-signup.component';
 import { BusinessReviewsComponent } from '../components/business-reviews/business-reviews.component';
+import { BusinessMenuComponent } from '../components/business-menu/business-menu.component';
 
 
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'business/register', component: BusinessSignupComponent},
     { path: 'business/reviews', component: BusinessReviewsComponent},
+    { path: 'business/menu', component: BusinessMenuComponent}
 
 ];
---
+++ src/components/business-menu/business-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-menu',
+  imports: [],
+  templateUrl: './business-menu.component.html',
+  styleUrl: './business-menu.component.css'
+})
+export class BusinessMenuComponent {
+
+}
