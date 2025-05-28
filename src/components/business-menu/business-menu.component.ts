76e1c223b052b35fa838083504df013e0736ac53
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-05-28 22:17:46 +0800

    menu

--- src/components/business-menu/business-menu.component.ts
+++ src/components/business-menu/business-menu.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-business-menu',
-  imports: [],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './business-menu.component.html',
   styleUrl: './business-menu.component.css'
 })
 export class BusinessMenuComponent {
 
+  showCheckboxes = false;
+
+  toggleCheckboxes(): void {
+    this.showCheckboxes = !this.showCheckboxes;
+  }
 }
