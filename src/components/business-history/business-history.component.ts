3ca33018ba5db3f4f835ded85e8dbbadb38f5712
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-28 21:07:35 +0800

    history component

--- src/components/business-history/business-history.component.ts
+++ src/components/business-history/business-history.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-business-history',
-  imports: [],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './business-history.component.html',
   styleUrl: './business-history.component.css'
 })
 export class BusinessHistoryComponent {
+  name: string = 'tinapayan'
+  historyDate: string = '01/01/2025'
+  historyName: string = 'Customer Name'
 
 }
