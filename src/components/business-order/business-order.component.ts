28a21fced1680f486cc4887a91c709f297d2afec
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-05-28 23:29:12 +0800

    orders

--- src/components/business-order/business-order.component.ts
+++ src/components/business-order/business-order.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-business-order',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './business-order.component.html',
   styleUrl: './business-order.component.css'
 })
 export class BusinessOrderComponent {
 
+   orderStates: { [key: number]: boolean } = {};
+
+  toggleOrder(item: number): void {
+    this.orderStates[item] = !this.orderStates[item];
+  }
+
+  isOrderOpen(item: number): boolean {
+    return this.orderStates[item] ?? false;
+  }
 }
