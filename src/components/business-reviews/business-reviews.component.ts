37d0f1add7d891e7d83fc9fcf9b64e204562c203
SenshiJeeyoh <jefflyrio.26@gmail.com>
2025-05-28 20:53:31 +0800

    business-review push

--- src/components/business-reviews/business-reviews.component.ts
+++ src/components/business-reviews/business-reviews.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CommonModule, formatCurrency } from '@angular/common';
 
 @Component({
   selector: 'app-business-reviews',
-  imports: [],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './business-reviews.component.html',
   styleUrl: './business-reviews.component.css'
 })
