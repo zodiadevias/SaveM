457866de71ced8270b51b293e34db778dd42a0cc
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-28 21:27:07 +0800

    business profile

--- src/components/business-profile/business-profile.component.ts
+++ src/components/business-profile/business-profile.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-business-profile',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './business-profile.component.html',
   styleUrl: './business-profile.component.css'
 })
 export class BusinessProfileComponent {
-
-}
+  whatAmI = 'storeowner';
+} 
