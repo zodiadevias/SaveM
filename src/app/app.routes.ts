aea556d277bec5096940ee944570ad1ebffd707f
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-05-28 19:58:26 +0800

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { BusinessSignupComponent } from '../components/business-signup/business-signup.component';
+import { BusinessReviewsComponent } from '../components/business-reviews/business-reviews.component';
 
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'business/register', component: BusinessSignupComponent}
+    { path: 'business/register', component: BusinessSignupComponent},
+    { path: 'business/reviews', component: BusinessReviewsComponent},
 
 ];
