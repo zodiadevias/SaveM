c65996855003e8f3cd13e2996120bee0590729d2
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-30 01:09:17 +0800

    frontend/ user-profile

--- src/components/user-profile/user-profile.component.ts
+++ src/components/user-profile/user-profile.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
