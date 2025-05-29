076760abbed3492b2a1b9f23c724349bf555c4bf
Lance-Ceo <abuyolancebenedict@gmail.com>
2025-05-30 00:17:55 +0800

    changes to map component

--- src/components/user-pickup/user-pickup.component.ts
+++ src/components/user-pickup/user-pickup.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-user-pickup',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MapComponent],
   templateUrl: './user-pickup.component.html',
   styleUrl: './user-pickup.component.css'
 })
 export class UserPickupComponent {
-
+  order: string = 'Banana Bread';
+  status: string = 'Pending';
 }
